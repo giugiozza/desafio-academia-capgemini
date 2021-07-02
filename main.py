3c14a86afa3953922d450a0a3dedb526ecf503d8
Giulia Giozza <giuliagiozza@gmail.com>
2021-07-02 14:50:27 -0300

    menu com tratamento de exceções

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from utility.utility import *
 
 
 def main():
-    menu()
+    while True:
+        try:
+            menu()
+        except ValueError:
+            print("Apenas números são válidos. Tente novamente.")
 
 
 if __name__ == '__main__':
--- utility/utility.py
+++ utility/utility.py
@@ -1,5 +1,16 @@
+import os
+
+
+def cadastrar_anuncio():
+    pass
+
+
+def gerar_relatorio():
+    pass
+
+
 def menu():
-    escolha = int(input("""
+    opcao = int(input("""
 ***** MENU DE ANÚNCIOS *****
 
   [ 1 ] Cadastrar Anúncio
@@ -7,4 +18,14 @@ def menu():
   [ 3 ] Sair
 
 Selecione uma opção: """))
-    return escolha
+
+    if opcao == 1:
+        cadastrar_anuncio()
+    elif opcao == 2:
+        gerar_relatorio()
+    elif opcao == 3:
+        print("Até mais!")
+        exit()
+    else:
+        print("--> Essa opção não é válida! :(\nTente novamente.\n")
+        menu()
