6b43aa0b1db76a303e463ed1fb3e0c9adc361b28
Giulia Giozza <giuliagiozza@gmail.com>
2021-07-02 14:20:11 -0300

    inclusão de menu principal

---
+++ main.py
@@ -0,0 +1,10 @@
+import sqlite3
+from utility.utility import *
+
+
+def main():
+    menu()
+
+
+if __name__ == '__main__':
+    main()
---
+++ utility/utility.py
@@ -0,0 +1,10 @@
+def menu():
+    escolha = int(input("""
+***** MENU DE ANÚNCIOS *****
+
+  [ 1 ] Cadastrar Anúncio
+  [ 2 ] Gerar Relatório
+  [ 3 ] Sair
+
+Selecione uma opção: """))
+    return escolha
