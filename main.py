3e20173fadb514d3837f34cd3b4f9d764fdd6114
Giulia Giozza <giuliagiozza@gmail.com>
2021-07-03 13:02:19 -0300

    menu com controller e retornos

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import sqlite3
-from utility.utility import *
+from utility.utilitarios_do_menu import *
 
 
 def main():
     while True:
         try:
-            menu()
+            opcao = menu()
+            if not opcao:
+                raise Exception
+            elif opcao == 1:
+                if cadastro_anuncio() == 1:
+                    cadastro_anuncio()
+                elif cadastro_anuncio() == 2:
+                    continue
+                else:
+                    print("Opção inválida! :(")
+            elif opcao == 2:
+                pass
+            elif opcao == 3:
+                print("\nAté mais!")
+                exit()
+            else:
+                print("\nOpção inválida. Selecione um número dentre as opções do menu.")
         except ValueError:
             print("Apenas números são válidos. Tente novamente.")
 
---
+++ utility/utilitarios_do_menu.py
@@ -0,0 +1,13 @@
+from utility.utilitarios_do_cadastro import *
+from utility.utilitarios_do_relatorio import *
+
+
+def menu():
+    return int(input("""
+***** MENU DE ANÚNCIOS *****
+
+[ 1 ] Cadastrar Anúncio
+[ 2 ] Gerar Relatório
+[ 3 ] Sair
+
+Selecione uma opção: """))
